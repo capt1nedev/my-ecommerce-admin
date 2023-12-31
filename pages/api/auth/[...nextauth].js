449bf42d22a4a10af07560f5307f9b6fd6c172ad
capt1nedev <justineobasibas@iskolarngbayan.pup.edu.ph>
2023-12-31 11:32:34 +0800

    return session even not admin email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -24,7 +24,7 @@ export const authOptions = {
             if (await isAdminEmail(session?.user?.email)) {
                 return session;
             } else {
-                return false;
+                return session;
             }
         },
     },
